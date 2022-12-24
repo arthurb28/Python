bef65c0a673f368bc3bb473a4bbc2a3cb2f42c86
ArthurBignon1 <103106470+ArthurBignon1@users.noreply.github.com>
2022-12-24 22:52:41 +0100

    Add files via upload

---
+++ test-arthur.py
@@ -0,0 +1,10 @@
+def produit_entiers(liste_entiers):
+    # écrivez le code ici
+    resultat = 1
+    for chiffres in liste_entiers :
+        resultat *= chiffres
+    return resultat 
+
+x = [1, 2, 3, 4]
+print(produit_entiers(x))
+
